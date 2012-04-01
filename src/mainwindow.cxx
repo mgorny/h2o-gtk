c5d775aa1e4150bc2537fccc9416603b83d6c34a
Michał Górny <mgorny@gentoo.org>
2012-04-01 14:45:42 +0200

    Support combo-box with function changing.

--- src/mainwindow.cxx
+++ src/mainwindow.cxx
@@ -11,7 +11,25 @@
 
 #include <stdexcept>
 
-#include <h2o>
+enum functions
+{
+	f_pT = 0,
+	f_ph,
+	f_ps
+};
+
+FunctionChoiceComboBox::FunctionChoiceComboBox()
+{
+	append("f(p, T)");
+	append("f(p, h)");
+	append("f(p, s)");
+
+	set_active(0);
+}
+
+FunctionChoiceComboBox::~FunctionChoiceComboBox()
+{
+}
 
 DataEntryPair::DataEntryPair(const char* desc, const char* unit,
 			double min, double max, double step, double pagestep,
@@ -21,18 +39,6 @@ DataEntryPair::DataEntryPair(const char* desc, const char* unit,
 	unit_label(unit, Gtk::ALIGN_START, Gtk::ALIGN_CENTER)
 {
 	get_adjustment()->configure(val, min, max, step, pagestep, 0);
-}
-
-DataEntryPair::DataEntryPair(const char* desc, const char* unit,
-			double min, double max, double step, double pagestep,
-			double decplaces)
-	: Gtk::SpinButton(0, decplaces),
-	label(desc, Gtk::ALIGN_END, Gtk::ALIGN_CENTER),
-	unit_label(unit, Gtk::ALIGN_START, Gtk::ALIGN_CENTER)
-{
-	get_adjustment()->configure(0, min, max, step, pagestep, 0);
-
-	set_editable(false);
 }
 
 DataEntryPair::~DataEntryPair()
@@ -46,8 +52,15 @@ void DataEntryPair::add_to_table(Gtk::Table& t, int row)
 	t.attach(unit_label, 2, 3, row, row + 1);
 }
 
+void DataEntryPair::remove_from_table(Gtk::Table& t)
+{
+	t.remove(label);
+	t.remove(*this);
+	t.remove(unit_label);
+}
+
 CalcBox::CalcBox()
-	: Gtk::Table(7, 3),
+	: Gtk::Table(8, 3),
 	p("p", "MPa", 1E-4, 100, 0.1, 5, 4, 10),
 	T("T", "K", 273.16, 2273.15, 1, 50, 2, 773.15),
 	v("v", "m³/kg", 0, 1E17, 0.01, 1, 4),
@@ -59,20 +72,16 @@ CalcBox::CalcBox()
 
 	recalc();
 
-	p.add_to_table(*this, 0);
-	T.add_to_table(*this, 1);
+	attach(func_chooser, 1, 2, 0, 1);
 
-	v.add_to_table(*this, 3);
-	u.add_to_table(*this, 4);
-	h.add_to_table(*this, 5);
-	s.add_to_table(*this, 6);
+	set_fields(p, T, v, u, h, s);
 
 	region_label.set_padding(0, 10);
 	region_label.set_alignment(Gtk::ALIGN_START, Gtk::ALIGN_END);
-	attach(region_label, 1, 3, 2, 3);
+	attach(region_label, 1, 3, 3, 4);
 
-	p.signal_value_changed().connect(sigc::mem_fun(*this, &CalcBox::recalc));
-	T.signal_value_changed().connect(sigc::mem_fun(*this, &CalcBox::recalc));
+	func_chooser.signal_changed().connect(sigc::mem_fun(*this, &CalcBox::reorder_fields));
+
 #if 0
 	v.signal_value_changed().connect(sigc::mem_fun(*this, &CalcBox::recalc));
 	u.signal_value_changed().connect(sigc::mem_fun(*this, &CalcBox::recalc));
@@ -85,58 +94,131 @@ CalcBox::~CalcBox()
 {
 }
 
+void CalcBox::remove_fields()
+{
+	conn1.disconnect();
+	conn2.disconnect();
+
+	p.remove_from_table(*this);
+	T.remove_from_table(*this);
+
+	v.remove_from_table(*this);
+	u.remove_from_table(*this);
+	h.remove_from_table(*this);
+	s.remove_from_table(*this);
+}
+
+void CalcBox::set_fields(DataEntryPair& in1, DataEntryPair& in2,
+		DataEntryPair& out1, DataEntryPair& out2,
+		DataEntryPair& out3, DataEntryPair& out4)
+{
+	in1.add_to_table(*this, 1);
+	in2.add_to_table(*this, 2);
+	in1.set_editable(true);
+	in2.set_editable(true);
+
+	conn1 = in1.signal_value_changed().connect(sigc::mem_fun(*this, &CalcBox::recalc));
+	conn2 = in2.signal_value_changed().connect(sigc::mem_fun(*this, &CalcBox::recalc));
+
+	out1.add_to_table(*this, 4);
+	out2.add_to_table(*this, 5);
+	out3.add_to_table(*this, 6);
+	out4.add_to_table(*this, 7);
+	out1.set_editable(false);
+	out2.set_editable(false);
+	out3.set_editable(false);
+	out4.set_editable(false);
+}
+
 void CalcBox::recalc()
 {
+	h2o::H2O medium;
+
 	try
 	{
-		h2o::H2O medium(p.get_value(), T.get_value());
-		const char* label_text;
-
-		switch (medium.region())
+		switch (func_chooser.get_active_row_number())
 		{
-			case 1:
-				label_text = "Region 1 (saturated water)";
+			case f_pT:
+				medium = h2o::H2O(p.get_value(), T.get_value());
 				break;
-			case 2:
-				label_text = "Region 2 (dry steam)";
+			case f_ph:
+				medium = h2o::H2O::ph(p.get_value(), h.get_value());
 				break;
-			case 3:
-				label_text = "Region 3 (supercritical)";
+			case f_ps:
+				medium = h2o::H2O::ps(p.get_value(), s.get_value());
 				break;
-			case 4:
-				label_text = "Region 4 (wet steam)";
-				break;
-			case 5:
-				label_text = "Region 5 (dry steam)";
-				break;
-			default:
-				label_text = "Unknown/invalid";
 		}
-		region_label.set_text(label_text);
-
-		v.set_value(medium.v());
-		u.set_value(medium.u());
-		h.set_value(medium.h());
-		s.set_value(medium.s());
 	}
 	catch (std::range_error)
 	{
 		region_label.set_text("Parameters out-of-range");
-
-		v.set_value(0);
-		u.set_value(0);
-		h.set_value(0);
-		s.set_value(0);
+		return;
 	}
 	catch (std::runtime_error)
 	{
 		region_label.set_text("Region not implemented");
-
-		v.set_value(0);
-		u.set_value(0);
-		h.set_value(0);
-		s.set_value(0);
+		return;
 	};
+
+	const char* label_text;
+	switch (medium.region())
+	{
+		case 1:
+			label_text = "Region 1 (saturated water)";
+			break;
+		case 2:
+			label_text = "Region 2 (dry steam)";
+			break;
+		case 3:
+			label_text = "Region 3 (supercritical)";
+			break;
+		case 4:
+			label_text = "Region 4 (wet steam)";
+			break;
+		case 5:
+			label_text = "Region 5 (dry steam)";
+			break;
+		default:
+			label_text = "Unknown/invalid";
+	}
+	region_label.set_text(label_text);
+
+	switch (func_chooser.get_active_row_number())
+	{
+		case f_pT:
+			h.set_value(medium.h());
+			s.set_value(medium.s());
+			break;
+		case f_ph:
+			T.set_value(medium.T());
+			s.set_value(medium.s());
+			break;
+		case f_ps:
+			T.set_value(medium.T());
+			h.set_value(medium.h());
+			break;
+	}
+
+	v.set_value(medium.v());
+	u.set_value(medium.u());
+}
+
+void CalcBox::reorder_fields()
+{
+	remove_fields();
+
+	switch (func_chooser.get_active_row_number())
+	{
+		case f_pT: // (p, T)
+			set_fields(p, T, v, u, h, s);
+			break;
+		case f_ph: // (p, h)
+			set_fields(p, h, T, v, u, s);
+			break;
+		case f_ps: // (p, s)
+			set_fields(p, s, T, v, u, h);
+			break;
+	}
 }
 
 MainWindow::MainWindow()
--- src/mainwindow.hxx
+++ src/mainwindow.hxx
@@ -8,11 +8,22 @@
 #ifndef _H2O_GTK_MAINWINDOW_HXX
 #define _H2O_GTK_MAINWINDOW_HXX 1
 
+#include <h2o>
+
 #include <sigc++/functors/mem_fun.h>
+#include <sigc++/connection.h>
+#include <gtkmm/comboboxtext.h>
 #include <gtkmm/label.h>
 #include <gtkmm/spinbutton.h>
 #include <gtkmm/table.h>
 #include <gtkmm/window.h>
+
+class FunctionChoiceComboBox : public Gtk::ComboBoxText
+{
+public:
+	FunctionChoiceComboBox();
+	virtual ~FunctionChoiceComboBox();
+};
 
 class DataEntryPair : public Gtk::SpinButton
 {
@@ -23,18 +34,19 @@ protected:
 public:
 	DataEntryPair(const char* desc, const char* unit,
 			double min, double max, double step, double pagestep,
-			double decplaces, double val);
-	DataEntryPair(const char* desc, const char* unit,
-			double min, double max, double step, double pagestep,
-			double decplaces);
+			double decplaces, double val = 0);
 	virtual ~DataEntryPair();
 
 	void add_to_table(Gtk::Table& t, int row);
+	void remove_from_table(Gtk::Table& t);
 };
 
 class CalcBox : public Gtk::Table
 {
+	sigc::connection conn1, conn2;
+
 protected:
+	FunctionChoiceComboBox func_chooser;
 	DataEntryPair p, T, v, u, h, s;
 	Gtk::Label region_label;
 
@@ -42,7 +54,12 @@ public:
 	CalcBox();
 	virtual ~CalcBox();
 
+	void remove_fields();
+	void set_fields(DataEntryPair& in1, DataEntryPair& in2,
+			DataEntryPair& out1, DataEntryPair& out2,
+			DataEntryPair& out3, DataEntryPair& out4);
 	void recalc();
+	void reorder_fields();
 };
 
 class MainWindow : public Gtk::Window
